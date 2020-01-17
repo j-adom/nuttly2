efac0fadacd2f0946f1e2bef7d1f33c383de20e9
Anna Thammakane <anna.thammakane@gmail.com>
2020-01-16 22:38:11 -0600

    deleted folder

---
+++ nuttly2/client/src/components/Product.js
@@ -0,0 +1,72 @@
+//Jordan wrote this
+//Need to update with correct api addresses not localhost
+
+import React, { Component } from 'react';
+import API from "../actions/storeActions";
+import BuyButton from './BuyButton';
+
+class Product extends Component {
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = { loading: true, product: {} }
+  // }
+
+  // async componentDidMount() {
+  //   let id = this.props.match.params.id
+  //   let response = await API.getProduct(id)
+  //   let data = await response.json()
+  //   this.setState({
+  //     loading: false,
+  //     product: data
+  //   })
+  // }
+
+  state = {
+    products: [],
+  };
+
+  componentDidMount(){
+      this.getProduct(id)
+  }
+  
+
+  getProducts = () => {
+    API.getProduct(id)
+      .then(res =>
+        this.setState({
+          products: res.data
+        })
+      )
+      .catch(() =>
+        this.setState({
+          product: [],
+          message: "No items found!"
+        })
+      );
+  };
+
+
+  render() {
+    if (!this.state.loading) {
+      return (
+        <div className="product">
+          <div className="product__information">
+            <h2 className="Product-title">{this.state.product.name}</h2>
+            <img src={`http://localhost:1337${this.state.product.imageURL}`} 
+                 alt={`${this.state.product.name}`}
+            />
+            <BuyButton {...this.state} />
+          </div>
+          <div className="product__description">
+            {this.state.product.description}
+          </div>
+        </div>
+      );
+    }
+
+    return (<h2>Waiting for API...</h2>);
+  }
+}
+
+export default Product;
